094f1c466cb54f34deb5d0e078629bc23c855863
Artem Rivnyi <123167556+ArtemRivnyi@users.noreply.github.com>
2026-06-08 19:28:54 +0100

    fix: add hard timeout and mutex to getBrowser to prevent indefinite freeze on Render

--- server/scrapers/puppeteerSetup.js
+++ server/scrapers/puppeteerSetup.js
@@ -4,15 +4,21 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 puppeteer.use(StealthPlugin());
 
 let browserInstance = null;
+let launchPromise = null;
 
 export async function getBrowser() {
   if (browserInstance) {
     return browserInstance;
   }
+  if (launchPromise) {
+    return launchPromise;
+  }
   
   console.log('[puppeteer] Launching hidden browser instance...');
-  browserInstance = await puppeteer.launch({
+  
+  const launchOptions = {
     headless: true,
+    timeout: 10000,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: [
       '--no-sandbox',
@@ -22,16 +28,29 @@ export async function getBrowser() {
       '--window-position=0,0',
       '--ignore-certifcate-errors',
       '--ignore-certifcate-errors-spki-list',
+      '--disable-dev-shm-usage',
     ],
     ignoreHTTPSErrors: true,
+  };
+
+  const pLaunch = puppeteer.launch(launchOptions);
+  const pTimeout = new Promise((_, reject) => setTimeout(() => reject(new Error('Puppeteer launch hard timeout')), 10000));
+
+  launchPromise = Promise.race([pLaunch, pTimeout]).then(browser => {
+    browserInstance = browser;
+    browserInstance.on('disconnected', () => {
+      console.log('[puppeteer] Browser disconnected, resetting instance...');
+      browserInstance = null;
+      launchPromise = null;
+    });
+    launchPromise = null;
+    return browserInstance;
+  }).catch(err => {
+    launchPromise = null;
+    throw err;
   });
 
-  browserInstance.on('disconnected', () => {
-    console.log('[puppeteer] Browser disconnected, resetting instance...');
-    browserInstance = null;
-  });
-
-  return browserInstance;
+  return launchPromise;
 }
 
 export async function closeBrowser() {
